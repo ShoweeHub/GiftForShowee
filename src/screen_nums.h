a50863dfafc9081cc412df1240ce8186a517d58f
skyatgit <1218980814@qq.com>
2024-03-12 01:00:04 +0800

    添加数字字库

---
+++ src/screen_nums.h
@@ -0,0 +1,72 @@
+constexpr static const bool screen_nums[10][5][3] = {
+        {
+                {true,  true,  true},
+                {true,  false, true},
+                {true,  false, true},
+                {true,  false, true},
+                {true,  true,  true}
+        },
+        {
+                {false, true,  false},
+                {true,  true,  false},
+                {false, true,  false},
+                {false, true,  false},
+                {true,  true,  true}
+        },
+        {
+                {true,  true,  true},
+                {false, false, true},
+                {true,  true,  true},
+                {true,  false, false},
+                {true,  true,  true}
+        },
+        {
+                {true,  true,  true},
+                {false, false, true},
+                {true,  true,  true},
+                {false, false, true},
+                {true,  true,  true}
+        },
+        {
+                {true,  false, true},
+                {true,  false, true},
+                {true,  true,  true},
+                {false, false, true},
+                {false, false, true}
+        },
+        {
+                {true,  true,  true},
+                {true,  false, false},
+                {true,  true,  true},
+                {false, false, true},
+                {true,  true,  true}
+        },
+        {
+                {true,  true,  true},
+                {true,  false, false},
+                {true,  true,  true},
+                {true,  false, true},
+                {true,  true,  true}
+        },
+        {
+                {true,  true,  true},
+                {false, false, true},
+                {false, false, true},
+                {false, false, true},
+                {false, false, true}
+        },
+        {
+                {true,  true,  true},
+                {true,  false, true},
+                {true,  true,  true},
+                {true,  false, true},
+                {true,  true,  true}
+        },
+        {
+                {true,  true,  true},
+                {true,  false, true},
+                {true,  true,  true},
+                {false, false, true},
+                {true,  true,  true}
+        }
+};
